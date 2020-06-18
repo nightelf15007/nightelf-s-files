af871ac3fd1b9b6797c822a62afdca5f8b4e6b91
nightelf15007 <62432276+nightelf15007@users.noreply.github.com>
2020-06-18 14:06:28 +0200

    Update password gen.py

--- Password generator/password gen.py
+++ Password generator/password gen.py
@@ -1,12 +1,20 @@
-import random #import module
-import string #import module (this is the one that makes the whole thing work)
-digits = random.randint(1, 11) #the total should be 12 characters and there should be at least digits and
-letters = 12 - digits           #the rest should be letters 
-charStr = ''.join((random.choice(string.digits) for i in range(digits))) #create a string containing the amount of random numbers randomly chosen 
-charStr += ''.join((random.choice(string.ascii_letters) for i in range(letters))) #add the amount of random letters randomly chosen to the string
-charList = list(charStr) #turn the string into a list of characters
-random.shuffle(charList) #shuffle the characters around  
-password = ''.join(charList) #save the new order into a variable
-print("Password: ", password) #print the password
+import random , string#import module
+symbols = 0 #declare variables
+low_letters = 0 #declare variables
+high_letters= 0 #declare variables
+digits = 0 #declare variables
+while low_letters <1 or high_letters <1 or digits <1 or symbols <1: #must contain at least one of each
+    symbols = random.randint(1,9) #has to leave 3 for the other types
+    low_letters = random.randint(1,(12 - symbols)) #random amount of what's left
+    high_letters= random.randint(1,(12 - low_letters + symbols)) #random amount of what's left
+    digits = 12 - (low_letters + high_letters + symbols) #the rest has to be symbols
+charStr = ''.join((random.choice(string.digits) for i in range(digits))) #make a string of random numbers
+charStr += ''.join((random.choice(string.ascii_lowercase) for i in range(low_letters))) #make a string of random lowercase letters
+charStr += ''.join((random.choice(string.ascii_uppercase) for i in range(high_letters))) #add to the string random uppercase letters
+charStr += ''.join((random.choice(string.punctuation) for i in range(symbols))) #add to the string random symbols
+charList = list(charStr) #make a list out of the string
+random.shuffle(charList) #mix the content of the list up
+password = ''.join(charList) #turn the list into a string again
+print("Password: ", password) #print out the password
 
 #Fun fact:59% of people use the same password everywhere
