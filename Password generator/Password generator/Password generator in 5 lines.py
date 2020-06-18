8d6eafd2755252b1934df6fb3fe778979e3c7aa6
nightelf15007 <62432276+nightelf15007@users.noreply.github.com>
2020-06-18 12:38:16 +0200

    Delete Password generator in 5 lines.py

--- Password generator/Password generator/Password generator in 5 lines.py
+++
@@ -1,7 +0,0 @@
-import string #imort module (this is abselute magic)
-import random #import module 
-chars=string.ascii_letters + string.digits + string.punctuation #choose characters from letters, numbers and symbols
-password = ''.join(random.choice(chars) for i in range(12)) #make a string out of the random characters
-print("Password: " + password) #print the string
-
-#Fun fact: It would take an attacker up to 7 months to bruteforce this password, assuming the keys per second is still 17042497.3kps
