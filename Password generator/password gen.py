ed50aa0a80b82c86cd3d86e90e30dea1bb780195
nightelf15007 <62432276+nightelf15007@users.noreply.github.com>
2020-06-18 11:46:24 +0200

    Add files via upload

---
+++ Password generator/password gen.py
@@ -0,0 +1,12 @@
+import random #import module
+import string #import module (this is the one that makes the whole thing work)
+digits = random.randint(1, 11) #the total should be 12 characters and there should be at least digits and
+letters = 12 - digits           #the rest should be letters 
+charStr = ''.join((random.choice(string.digits) for i in range(digits))) #create a string containing the amount of random numbers randomly chosen 
+charStr += ''.join((random.choice(string.ascii_letters) for i in range(letters))) #add the amount of random letters randomly chosen to the string
+charList = list(charStr) #turn the string into a list of characters
+random.shuffle(charList) #shuffle the characters around  
+password = ''.join(charList) #save the new order into a variable
+print("Password: ", password) #print the password
+
+#Fun fact: It would take an attacker up to 7 months to bruteforce this password, assuming the keys per second is still 17042497.3kps
